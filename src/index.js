17bcb5ab61bd681ef738bd37a7df48b6424028fb
Melnyk675 <113244781+Melnyk675@users.noreply.github.com>
2023-03-20 13:53:56 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -132,7 +132,7 @@ function notification(length, totalHits) {
     Notiflix.Notify.success(`Hooray! We found ${totalHits} images.`);
   }
 
-  if (totalHits <= total || totalHits === 0) {
+  if (totalHits <= total) {
     refs.btnLoadMore.style.display = 'none'; 
     
     Notiflix.Notify.info(
